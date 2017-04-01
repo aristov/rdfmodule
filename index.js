b075fd08e41505fadd03961ced7cb558414408a4
aristov <vv.aristov@gmail.com>
2017-04-02 02:52:29 +0300

    add sources

---
+++ index.js
@@ -0,0 +1,25 @@
+//const { RDFDocumentAssembler, RDFDOMAssembler } = require('./lib')
+const fs = require('fs')
+const ttl = fs.readFileSync('src/rdf-schema.ttl', 'utf-8')
+
+/*const N3 = require('n3')
+const parser = N3.Parser()
+const ttl = fs.readFileSync('src/22-rdf-syntax-ns.ttl', 'utf-8')
+const arr = []
+parser.parse(ttl,
+    (error, triple, prefixes) => {
+        if(triple) arr.push(
+            `<${ triple.subject }>`, triple.object, triple.predicate
+        )
+        else console.log(arr, prefixes)
+    })*/
+
+const RDFS_NAMESPACE_URI = 'http://www.w3.org/2000/01/rdf-schema#'
+const TURTLE_MIME_TYPE = 'text/turtle'
+const RDFXML_MIME_TYPE = 'application/rdf+xml'
+
+const rdflib = require('rdflib')
+const store = rdflib.graph()
+rdflib.parse(ttl, store, RDFS_NAMESPACE_URI, TURTLE_MIME_TYPE)
+const cb = (...res) => console.log(res)
+rdflib.serialize(store, new rdflib.IndexedFormula(), RDFS_NAMESPACE_URI, RDFXML_MIME_TYPE, cb)
